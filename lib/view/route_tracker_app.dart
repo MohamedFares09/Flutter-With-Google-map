10e340cc0d61d271dbac4507905f94b524ba6c5c
MohamedFares09 <mofares010@gmail.com>
2026-01-28 01:45:44 +0200

    update getCurrentLocation : get location data and add marker

--- lib/view/route_tracker_app.dart
+++ lib/view/route_tracker_app.dart
@@ -12,25 +12,49 @@ class RouteTrakerApp extends StatefulWidget {
 class _RouteTrakerAppState extends State<RouteTrakerApp> {
   late CameraPosition initCameraPostion;
   late LocationService locationService;
+  late GoogleMapController googleMapController;
+  Set<Marker> markers = {};
   @override
   void initState() {
     initCameraPostion = CameraPosition(target: LatLng(0, 0));
     locationService = LocationService();
-    getCurrentLocation();
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      markers: markers,
       zoomControlsEnabled: false,
       initialCameraPosition: initCameraPostion,
+      onMapCreated: (controller) {
+        googleMapController = controller;
+        getCurrentLocation();
+      },
     );
   }
 
   void getCurrentLocation() async {
     try {
-      var currentLocation = await locationService.getLocation();
+      var locationData = await locationService.getLocation();
+      var currentPostion = LatLng(
+        locationData.latitude!,
+        locationData.longitude!,
+      );
+      Marker currentMarker = Marker(
+        markerId: MarkerId("marker id"),
+        position: currentPostion,
+      );
+      markers.add(currentMarker);
+      setState(() {});
+      CameraPosition cameraPosition = CameraPosition(
+        target: currentPostion,
+        zoom: 15,
+      );
+      googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(cameraPosition),
+      );
     } on LocationServerException catch (e) {
       // TODO
     } on LocationPermesstionException catch (e) {
@@ -40,3 +64,8 @@ class _RouteTrakerAppState extends State<RouteTrakerApp> {
     }
   }
 }
+
+// create text field 
+// listen to the text field 
+// search place 
+// display result 
